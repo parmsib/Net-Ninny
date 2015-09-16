bf86293c99c78e4d55a68856a4dfcec08b172ac7
robertsvensk <robertsvensk91@gmail.com>
2015-09-16 18:17:07 +0200

    client forward message

--- client.c
+++ client.c
@@ -1,39 +1,90 @@
-
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 #include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
 
 #define PORT "3490" // Port to connect to
+
 #define MAXDATASIZE 100
 
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+  if (sa->sa_family == AF_INET){
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
-int main (int argc, char *argv[]){
+int main (int argc, char *argv[])
+{
 	printf("Client started\n");
 
-  int status;
-  struct addrinfo hints;
-  struct addrinfo *servinfo;
-	
+  int servsfd, hostfd, numbytes;
+  char buf[MAXDATASIZE];
+  struct addrinfo hints, *servinfo, *p;
+  int status; // Return value
+  char s[INET6_ADDRSTRLEN];
+
+  if (argc != 2) {
+    fprintf(stderr,"usage: client hostname\n");
+    exit(1);
+  }
+
   memset(&hints, 0, sizeof hints); // make sure it is empty
   hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
   hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-	if((status = getaddrinfo(NULL,
-                  PORT,
-                  *hints,
-                  *servinfo) != 0){
-      fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-      exit(1);
+  if((status = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    return 1;
   }
 
-  servsfd = socket(servinfo->ai_family, 
-                  servinfo->ai_socktype, 
-                  servinfo->ai_protocol);
 
-  connect(servsfd, servinfo->ai_addr, servinfo->ai_addrlen);
+  for(p = servinfo; p != NULL; p = p->ai_next){
+    if ((servsfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+      perror("client: socket");
+      continue;
+    }
+
+    if (connect(servsfd, p->ai_addr, p->ai_addrlen) == -1){
+      close(servsfd);
+      perror("client: connect");
+      continue;
+    }
+
+    break;
+  }
+
+  if(p == NULL){
+    fprintf(stderr, "client: failed to connect\n");
+    return 2;
+  }
+
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
+  printf("client: connecting to %s\n", s);
   
+  char HTTP[] = "I'm a client!";
+  if (send(servsfd, HTTP, sizeof HTTP, 0) == -1)
+    perror("send");
 
+  freeaddrinfo(servinfo); // all done with this structure
 
+  if((numbytes = recv(servsfd, buf, MAXDATASIZE-1,0)) == -1) {
+    perror("recv");
+    exit(1);
+  }
+
+  buf[numbytes] = '\0';
+
+  printf("client: recieved '%s'\n",buf);
+  close(servsfd);
+  return 0;
 }
