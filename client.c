85637ee3021ff8d2d78af6268fd17c2cc64d5b61
parmsib <parsib01@hotmail.com>
2015-09-20 14:20:22 +0100

    added http and changed GET recv from browser

--- client.c
+++ client.c
@@ -13,122 +13,136 @@
 #include "client.h"
 
 #include "util.h"
+#include "http.h"
 
 void client_init_hints(struct addrinfo* hints){
-  memset(hints, 0, sizeof hints); // make sure it is empty
-  hints->ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
-  hints->ai_socktype = SOCK_STREAM; // TCP stream sockets
+    memset(hints, 0, sizeof hints); // make sure it is empty
+    hints->ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+    hints->ai_socktype = SOCK_STREAM; // TCP stream sockets
 }
 
 void client_handle_request(int browser_fd){
     //entry point from server
+    char buf[MAXDATASIZE];
 
-    //some dummy code
-  char buf[MAXDATASIZE];
-  int numbytes;
+    fcntl(browser_fd, F_SETFL, O_NONBLOCK);
+    
+    int GET_size = 0;
+    while(1){
+        //recv whole header
+        int res = recv(browser_fd, buf + GET_size, MAXDATASIZE-1,0);
+        if(res < 0){
+            perror("recv from browser failed.");
+            return -1;
+        }
+        GET_size += res;
+        if (http_whole_header(buf))
+            //buf contains the whole header (request)
+            break;
+    }
 
-  if((numbytes = recv(browser_fd, buf, MAXDATASIZE-1,0)) == -1)
-  {
-    perror("recv");
-    exit(1);
-  }
+    //  if((numbytes = recv(browser_fd, buf, MAXDATASIZE-1,0)) == -1)
+    //  {
+    //    perror("recv");
+    //    exit(1);
+    //  }
 
-  buf[numbytes] = '\0';
+    buf[numbytes] = '\0';
 
-  printf("---------------------------server: recieved from browser \n'%s'\n",buf);
+    printf("---------------------------server: recieved from browser \n'%s'\n",buf);
 
-  char *HOST;
-  char *point;
-  char playbuf[MAXDATASIZE];
+    char *HOST;
+    char *point;
+    char playbuf[MAXDATASIZE];
 
-  memcpy(playbuf,buf,numbytes);
+    memcpy(playbuf,buf,numbytes);
 
-  if(point = strstr(playbuf, "Host:")){
-    printf("FOUND*********\n");
-    char *token = strtok(point," ");
-    token = strtok(NULL,"\n");
-    token[strlen(token)-1] ='\0';
-    HOST = token;
-  } else {
-    printf("HOST NAME NOT FOUND ***********");
-  }
-  // if (send(browser_fd, "Hello, world!", 13, 0) == -1)
-  //   perror("send");
+    if(point = strstr(playbuf, "Host:")){
+        printf("FOUND*********\n");
+        char *token = strtok(point," ");
+        token = strtok(NULL,"\n");
+        token[strlen(token)-1] ='\0';
+        HOST = token;
+    } else {
+        printf("HOST NAME NOT FOUND ***********");
+    }
+    // if (send(browser_fd, "Hello, world!", 13, 0) == -1)
+    //   perror("send");
 
     //end of dummy code
 
     //TODO: make "main" below into this function
-  printf("Client side started\n");
+    printf("Client side started\n");
 
-  int hostfd;
-  struct addrinfo hints;
-  client_init_hints(&hints);
+    int hostfd;
+    struct addrinfo hints;
+    client_init_hints(&hints);
 
-  struct addrinfo* hostinfo;
-  int rv;
-  if((rv = getaddrinfo(HOST,HOSTPORT,&hints,&hostinfo)) != 0){
-    fprintf(stderr, "getaddrinfo: %s\n",gai_strerror(rv));
-    return 1;
-  }
+    struct addrinfo* hostinfo;
+    int rv;
+    if((rv = getaddrinfo(HOST,HOSTPORT,&hints,&hostinfo)) != 0){
+        fprintf(stderr, "getaddrinfo: %s\n",gai_strerror(rv));
+        return 1;
+    }
 
-      //Create socket to port and connct to host
-  int host_sock_fd;
-  host_sock_fd = client_connect_host(hostinfo);
-  freeaddrinfo(hostinfo);
+    //Create socket to port and connct to host
+    int host_sock_fd;
+    host_sock_fd = client_connect_host(hostinfo);
+    freeaddrinfo(hostinfo);
 
-  //Forward HTTP to host(msg)
-   printf("---------------------------client: send to host \n'%s'\n",buf);
-  if (send(host_sock_fd, buf, sizeof buf, 0) == -1)
-    perror("send");
+    //Forward HTTP to host(msg)
+    printf("---------------------------client: send to host \n'%s'\n",buf);
+    if (send(host_sock_fd, buf, GET_size, 0) == -1)
+        perror("send");
 
-  // Recive response from host
-  if((numbytes = recv(host_sock_fd, buf, MAXDATASIZE-1,0)) == -1) {
-    perror("recv");
-    exit(1);
-  }
+    // Recive response from host
+    if((numbytes = recv(host_sock_fd, buf, MAXDATASIZE-1,0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
 
-  printf("%d",numbytes);
-  buf[numbytes] = '\0';
+    printf("%d",numbytes);
+    buf[numbytes] = '\0';
 
-  printf("-------------------------------client: recieved from host \n'%s'\n",buf);
-  close(host_sock_fd);
+    printf("-------------------------------client: recieved from host \n'%s'\n",buf);
+    close(host_sock_fd);
 
 
-  //Forward response to browser
-  printf("-------------------------------client: send to browser \n'%s'\n",buf);
-  if (send(browser_fd, buf, sizeof buf, 0) == -1)
-    perror("send");
+    //Forward response to browser
+    printf("-------------------------------client: send to browser \n'%s'\n",buf);
+    if (send(browser_fd, buf, sizeof buf, 0) == -1)
+        perror("send");
 
-  return 0;
+    return 0;
 
 }
 
 int client_connect_host(struct addrinfo* hostinfo){
-  struct addrinfo* p;
-  int hostfd;
-  char s[INET6_ADDRSTRLEN];
-  for(p = hostinfo; p != NULL; p = p->ai_next){
-    if ((hostfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
-      perror("client: socket");
-      continue;
+    struct addrinfo* p;
+    int hostfd;
+    char s[INET6_ADDRSTRLEN];
+    for(p = hostinfo; p != NULL; p = p->ai_next){
+        if ((hostfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(hostfd, p->ai_addr, p->ai_addrlen) == -1){
+            close(hostfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
     }
 
-    if (connect(hostfd, p->ai_addr, p->ai_addrlen) == -1){
-      close(hostfd);
-      perror("client: connect");
-      continue;
+    if(p == NULL){
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
     }
 
-    break;
-  }
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
+    printf("client: connecting to %s\n", s);
 
-  if(p == NULL){
-    fprintf(stderr, "client: failed to connect\n");
-    return 2;
-  }
-
-  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
-  printf("client: connecting to %s\n", s);
-
-  return hostfd;
+    return hostfd;
 }
